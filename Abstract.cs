f1882a5d86f472372439f6333b92d37d0cd71100
aptem1223 <sanstop357@gmail.com>
2025-03-20 11:17:40 +0200

    Add files via upload

---
+++ Abstract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    abstract class Abstract
+    {
+        public void Method1()=>
+            Console.WriteLine("ОБЫЧНЫЙ МЕТОД METHOD 1 ИЗ Абстрактный класса Abstract ");
+        public void Method2() =>
+            Console.WriteLine("ВУРТУАЛЬНЫЙ МЕТОД METHOD 2 ИЗ Абстрактный класса Abstract ");
+        public abstract void Method3();
+        public abstract void Method4();
+
+    }
+}
---
+++ Derived.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Derived
+    {
+        public override void Method2()
+        {
+            base.Method2;
+            Console.WriteLine("МЕТОД Method2 КЛАССА Derived ");
+        }
+        
+    }
+}
